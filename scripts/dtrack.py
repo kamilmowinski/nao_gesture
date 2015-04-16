69666a0f3c396b46c278cfa9a89a348102a0cadc
Your Name <you@example.com>
2015-04-16 10:43:37 +0200

    Nao_hello

--- scripts/dtrack.py
+++ scripts/dtrack.py
@@ -13,8 +13,6 @@ class Tracker():
 	rospy.init_node('tracker_mykinect', anonymous=True)
 	self.myFrame = 'kinect_link'
 	self.r = rospy.Rate(20)		#rospy.Rate(1) = 1Hz
-
-
 	self.tf = tf.TransformListener()
 	rospy.loginfo("Start tracking for 3s...")
         rospy.sleep(3.0)
@@ -33,7 +31,46 @@ class Tracker():
 			break
 		except:
 			pass
+
+	ip = rospy.get_param('~ip', '10.104.16.141')
+	port = int(rospy.get_param('~port', '9559'))
+	self.al = ALProxy("ALAutonomousLife", ip, port)
+	self.postureProxy = ALProxy("ALRobotPosture", ip, port)
+	self.tts = ALProxy("ALTextToSpeech", ip, port)
+	self.motionProxy = ALProxy("ALMotion", ip, port)
+	self.al.setState("disabled")
+	self.postureProxy.goToPosture("StandInit", 0.5)
+	for part in ["Head", "LArm", "RArm"]:
+		self.motionProxy.setStiffnesses(part, 1.0)
+	self.init_kinnect_pose()
+
+    def init_kinnect_pose(self):
+	self.motionProxy.setAngles(['LShoulderRoll', 'RShoulderRoll'], self.to_rad([57.9, -57.9]), 1.0);
+	self.motionProxy.setAngles(['LShoulderPitch', 'RShoulderPitch'], self.to_rad([-94.1, -94.1]), 1.0);
+	self.motionProxy.setAngles(['LElbowYaw', 'RElbowYaw'], self.to_rad([-11.5, 11.5]), 1.0);
+	self.motionProxy.setAngles(['LElbowRoll', 'RElbowRoll'], self.to_rad([-61.3, 61.3]), 1.0);
+	self.motionProxy.setAngles(['LWristYaw', 'RWristYaw'], self.to_rad([7.0, -7.0]), 1.0);
+	self.motionProxy.setAngles(['LHand', 'RHand'], self.to_rad([0.99, 0.99]), 1.0);
+
+    def to_rad(self, angles):
+	return map(lambda x: float(x)*math.pi/180.0, angles)
+
 	
+
+    def nao_hello(self)
+	self.postureProxy.goToPosture("Standing",1)
+	self.motionProxy.setAngles('RShoulderPitch',self.to_rad(-60),1.0)
+	self.motionProxy.setAngles('RShoulderRoll',self.to_rad(-1.6),1.0)
+	self.motionProxy.setAngles('RElbowRoll',self.to_rad(37.5),1.0)
+	self.motionProxy.setAngles('RElbowYaw',self.to_rand(79.1),1.0)
+	self.motionProxy.setAngles('RWirstYaw',self.to_rand(57),1.0)
+	self.motionProxy.openHand('RHand')
+	self.tts.say("Hello my friend")
+	for i in range(1,2):
+		self.motionProxy.setAngles('RShoulderPitch',self.to_rad(-25),2.0)
+		self.motionProxy.setAngles('RShoulderPitch',self.to_rad(-60),1.0)
+    
+
 
     def hello_process(self):
 	self.count = 0
@@ -58,6 +95,7 @@ class Tracker():
 					self.side = 0
 		if (self.count >= 4):
 			print('HELLO')
+			self.nao_hello()
 			self.count = 0
 		self.r.sleep()		#sleep rospy.Rate
 
@@ -210,10 +248,10 @@ class Tracker():
 if __name__ == '__main__':
     try:
         tr = Tracker()
-	#tr.hello_process()
+	tr.hello_process()
 	#tr.sensei_process()
 	#tr.goto_process()
-	tr.tome_process()
+	#tr.tome_process()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
