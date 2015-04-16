1fa2173fcc9910e1139b147d51820a530d201815
PatrykBogd <patrykbochun@gmail.com>
2015-04-16 13:05:10 +0200

    Update Nao_hello and Nao_sensei

--- scripts/dtrack.py
+++ scripts/dtrack.py
@@ -16,39 +16,20 @@ class Tracker():
 	self.myFrame = 'kinect_link'
 	self.r = rospy.Rate(20)		#rospy.Rate(1) = 1Hz
 
-	ip = rospy.get_param('~ip', '10.104.16.50')
-	port = int(rospy.get_param('~port', '9559'))
-	#self.al = ALProxy("ALAutonomousLife", ip, port)
-	#self.postureProxy = ALProxy("ALRobotPosture", ip, port)
-	#self.motionProxy = ALProxy("ALMotion", ip, port)
-	#self.al.setState("disabled")
-	#self.postureProxy.goToPosture("StandInit", 0.5)
-	#for part in ["Head", "LArm", "RArm"]:
-	#	self.motionProxy.setStiffnesses(part, 1.0)
-	#	self.init_kinnect_pose()
+#Skeleton Listener
 	self.tf = tf.TransformListener()
-
-
-	ip = rospy.get_param('~ip', '10.104.16.50')
-	port = int(rospy.get_param('~port', '9559'))
-	self.al = ALProxy("ALAutonomousLife", ip, port)
-	self.postureProxy = ALProxy("ALRobotPosture", ip, port)
-	self.tts = ALProxy("ALTextToSpeech", ip, port)
-	self.motionProxy = ALProxy("ALMotion", ip, port)
-	self.al.setState("disabled")
-	self.postureProxy.goToPosture("StandInit", 0.5)
-	for part in ["Head", "LArm", "RArm"]:
-		self.motionProxy.setStiffnesses(part, 1.0)
+#Nao init and connect
+	self.nao_init()
 
 	rospy.loginfo("Start tracking for 3s...")
         rospy.sleep(3.0)
 	rospy.loginfo("Tracking started!")
-	self.hoh = 0.86 #wysokosc na jakiej znajduje sie kinect
+	self.hoh = 0.86 #Nao Hight in meters
 	for i in range(1, 4):	
+#Get all nessecery joins. (Right side = Left side if you stand front to sensor)		
 		try:
-			#self.target_joint = "{0}_{1}".format(rospy.get_param('~target_joint', '/head'), i)	#uzysukuje nazwe zalezna od szkieletu
 			self.j_head = "{0}_{1}".format(rospy.get_param('~target_joint', '/head'), i)
-			self.j_l_hand = "{0}_{1}".format(rospy.get_param('~target_joint', '/left_hand'), i) #widzi odbicie lustrzane prawa to lewa
+			self.j_l_hand = "{0}_{1}".format(rospy.get_param('~target_joint', '/left_hand'), i) 
 			self.j_r_hand = "{0}_{1}".format(rospy.get_param('~target_joint', '/right_hand'), i)
 			self.j_l_shoul = "{0}_{1}".format(rospy.get_param('~target_joint', '/left_shoulder'), i)
 			self.j_torso = "{0}_{1}".format(rospy.get_param('~target_joint', '/torso'), i)
@@ -57,74 +38,65 @@ class Tracker():
 			break
 		except:
 			pass
+    
+    def toRAD(self,angle):
+	return angle*almath.TO_RAD
 
-	
-	#self.init_kinnect_pose()
+    def nao_init(self):
+	#NAO CONNECT AND INIT
+	# PORT AND IP NAO ROBOT
+	ip = rospy.get_param('~ip', '10.104.16.50')
+	port = int(rospy.get_param('~port', '9559'))
 
-    def init_kinnect_pose(self):
-	self.motionProxy.setAngles(['LShoulderRoll', 'RShoulderRoll'], self.to_rad([57.9, -57.9]), 1.0);
-	self.motionProxy.setAngles(['LShoulderPitch', 'RShoulderPitch'], self.to_rad([-94.1, -94.1]), 1.0);
-	self.motionProxy.setAngles(['LElbowYaw', 'RElbowYaw'], self.to_rad([-11.5, 11.5]), 1.0);
-	self.motionProxy.setAngles(['LElbowRoll', 'RElbowRoll'], self.to_rad([-61.3, 61.3]), 1.0);
-	self.motionProxy.setAngles(['LWristYaw', 'RWristYaw'], self.to_rad([7.0, -7.0]), 1.0);
-	self.motionProxy.setAngles(['LHand', 'RHand'], self.to_rad([0.99, 0.99]), 1.0);
-
-    def to_rad(self, angles):
-	return map(lambda x: float(x)*math.pi/180.0, angles)
-
-	
-    def init_kinnect_pose(self):
-	self.motionProxy.setAngles(['LShoulderRoll', 'RShoulderRoll'], self.to_rad([57.9, -57.9]), 1.0);
-	self.motionProxy.setAngles(['LShoulderPitch', 'RShoulderPitch'], self.to_rad([-94.1, -94.1]), 1.0);
-	self.motionProxy.setAngles(['LElbowYaw', 'RElbowYaw'], self.to_rad([-11.5, 11.5]), 1.0);
-	self.motionProxy.setAngles(['LElbowRoll', 'RElbowRoll'], self.to_rad([-61.3, 61.3]), 1.0);
-	self.motionProxy.setAngles(['LWristYaw', 'RWristYaw'], self.to_rad([7.0, -7.0]), 1.0);
-	self.motionProxy.setAngles(['LHand', 'RHand'], self.to_rad([0.99, 0.99]), 1.0);
-
-    def to_rad(self, angles):
-	return map(lambda x: float(x)*math.pi/180.0, angles)
+	self.al = ALProxy("ALAutonomousLife", ip, port)
+	self.postureProxy = ALProxy("ALRobotPosture", ip, port)
+	self.tts = ALProxy("ALTextToSpeech", ip, port)
+	self.motionProxy = ALProxy("ALMotion", ip, port)
+	self.al.setState("disabled")
+	self.postureProxy.goToPosture("StandInit", 0.5)	
 
     def nao_hello(self):
-	self.postureProxy.goToPosture("Standing",1)
-	self.motionProxy.setAngles('RShoulderPitch',self.to_rad([-97.3])[0],1.0)
-	self.motionProxy.setAngles('RShoulderRoll',self.to_rad([-39.2])[0],1.0)
-	self.motionProxy.setAngles('RElbowRoll',self.to_rad([50.8])[0],1.0)
-	self.motionProxy.setAngles('RElbowYaw',self.to_rad([8.5])[0],1.0)
-	self.motionProxy.setAngles('RWristYaw',self.to_rad([-13.4])[0],1.0)
+#Nao hello module
+	self.postureProxy.goToPosture("Standing",1.0)
+	self.motionProxy.setAngles('RShoulderPitch',self.toRAD(-97.3),1.0)
+	self.motionProxy.setAngles('RShoulderRoll',self.toRAD(-39.2),1.0)
+	self.motionProxy.setAngles('RElbowRoll',self.toRAD(50.8),1.0)
+	self.motionProxy.setAngles('RElbowYaw',self.toRAD(8.5),1.0)
+	self.motionProxy.setAngles('RWristYaw',self.toRAD(-13.4),1.0)
 	self.motionProxy.openHand('RHand')
 	self.tts.say("Hello my friend")
 	for i in range(0,2):
-		#self.motionProxy.setAngles('RShoulderRoll',self.to_rad([-12.2])[0],1.0)
-		#self.motionProxy.setAngles('RShoulderRoll',self.to_rad([-39.2])[0],1.0)
-		#self.motionProxy.setAngles('RShoulderRoll',self.to_rad([-60.4])[0],1.0)
-		self.motionProxy.angleInterpolation("RShoulderRoll",-12.2*almath.TO_RAD,0.3,True)
-		self.motionProxy.angleInterpolation("RShoulderRoll",39.2*almath.TO_RAD,0.3,True)
-		self.motionProxy.angleInterpolation("RShoulderRoll",-50.4*almath.TO_RAD,0.3,True)
-	self.postureProxy.goToPosture("Standing",1)
+		self.motionProxy.angleInterpolation("RShoulderRoll",self.toRAD(-12.2),0.3,True)
+		self.motionProxy.angleInterpolation("RShoulderRoll",self.toRAD(39.2),0.3,True)
+		self.motionProxy.angleInterpolation("RShoulderRoll",self.toRAD(-50.4),0.3,True)
+	self.postureProxy.goToPosture("Standing",1.0)
 
 
     def nao_sensei(self):
-	self.postureProxy.goToPosture("Standing",1)
-	self.motionProxy.setAngles(['RShoulderPitch','LShoulderPitch'],[52.7*almath.TO_RAD,49.1*almath.TO_RAD],1.0)
-	self.motionProxy.setAngles(['RShoulderRoll','LShoulderRoll'],[13.3*almath.TO_RAD,-8.9*almath.TO_RAD],1.0)
-	self.motionProxy.setAngles(['RElbowRoll','LElbowRoll'],[88.3*almath.TO_RAD,-87.6*almath.TO_RAD],1.0)
-	self.motionProxy.setAngles(['RElbowYaw','LElbowYaw'],[68.1*almath.TO_RAD,-60.9*almath.TO_RAD],1.0)
-	self.motionProxy.setAngles(['RWristYaw','LWristYaw'],[-10.3*almath.TO_RAD,11.0*almath.TO_RAD],1.0)
-	self.motionProxy.setAngles(['RHand','LHand'],[-10.3*almath.TO_RAD,11.0*almath.TO_RAD],1.0)
+#Nao hello(sensei) module
+	self.postureProxy.goToPosture("Standing",1.0)
+	self.motionProxy.setAngles(['RShoulderPitch','LShoulderPitch'],[self.toRAD(52.7),self.toRAD(49.1)],1.0)
+	self.motionProxy.setAngles(['RShoulderRoll','LShoulderRoll'],[self.toRAD(13.3),self.toRAD(-8.9)],1.0)
+	self.motionProxy.setAngles(['RElbowRoll','LElbowRoll'],[self.toRAD(88.3),self.toRAD(-87.6)],1.0)
+	self.motionProxy.setAngles(['RElbowYaw','LElbowYaw'],[self.toRAD(68.1),self.toRAD(-60.9)],1.0)
+	self.motionProxy.setAngles(['RWristYaw','LWristYaw'],[self.toRAD(-10.3),self.toRAD(11.0)],1.0)
+	self.motionProxy.setAngles(['RHand','LHand'],[self.toRAD(-10.3),self.toRAD(11.0)],1.0)
 	self.motionProxy.openHand('RHand')
 	self.motionProxy.openHand('LHand')
 	#sequence head
 	self.tts.say("Domo Arigato")
 	#self.motionProxy.angleInterpolation("HeadPitch",0.0,1.0,True)
-	self.motionProxy.angleInterpolation("HeadPitch",26.5*almath.TO_RAD,1.0,True)
-	self.motionProxy.angleInterpolation("HeadPitch",0.0*almath.TO_RAD,1.0,True)
+	self.motionProxy.angleInterpolation("HeadPitch",self.toRAD(26.5),1.0,True)
+	self.motionProxy.angleInterpolation("HeadPitch",0.0,1.0,True)
 	
 	self.postureProxy.goToPosture("Standing",1)
 	
     def hello_process(self):
+#HELLO module
 	self.count = 0
 	self.side = 0		#left 0 right 1
 	while not rospy.is_shutdown():
+# Get all nessecery vectors
 		trans_head, rot= self.tf.lookupTransform('/openni_depth_frame', self.j_head, rospy.Duration())
 		vec_head = Vector3(*trans_head)
 		trans_hand, rot = self.tf.lookupTransform('/openni_depth_frame', self.j_l_hand, rospy.Duration())
@@ -133,17 +105,19 @@ class Tracker():
 		vec_shoul = Vector3(*trans_shoul)
 		trans_torso, rot = self.tf.lookupTransform('/openni_depth_frame', self.j_torso, rospy.Duration())
 		vec_torso = Vector3(*trans_torso)
+# IF right hand upper that torso
 		if (vec_hand.z > vec_torso.z):
-			#print('reka w gorze')
-			#print(vec_shoul.y-vec_hand.y)
+	#left side
 			if (not self.side):
 				if (vec_shoul.y-vec_hand.y > 0.1):
 					self.count+=1
 					self.side = 1
+	#right side		
 			else:
 				if (vec_shoul.y-vec_hand.y < (-0.2) ):
 					self.count+=1
 					self.side = 0
+	#Hello start		
 		if (self.count >= 4):
 			print('HELLO')
 			self.nao_hello()
@@ -151,7 +125,9 @@ class Tracker():
 		self.r.sleep()		#sleep rospy.Rate
 
     def sensei_process(self):
+#HELLO(SENSEI) Module
 	while not rospy.is_shutdown():
+#Get all nessecery Vectors
 		trans_head, rot = self.tf.lookupTransform('/openni_depth_frame', self.j_head, rospy.Duration())
 		trans_rh, rot = self.tf.lookupTransform('/openni_depth_frame', self.j_r_hand, rospy.Duration())
 		trans_lh, rot = self.tf.lookupTransform('/openni_depth_frame', self.j_l_hand, rospy.Duration())
@@ -160,23 +136,17 @@ class Tracker():
 		vec_rh = Vector3(*trans_rh)
 		vec_lh = Vector3(*trans_lh)
 		vec_tor = Vector3(*trans_tor)
-		#print(str(vec_head.x) + "\t\t" + str(vec_head.y) + "\t\t" + str(vec_head.z))
-		#print(str(vec_rh.x) + "\t\t" + str(vec_rh.y) + "\t\t" + str(vec_rh.z))
-		#print(str(vec_lh.x) + "\t\t" + str(vec_lh.y) + "\t\t" + str(vec_lh.z))
-		#print(str(vec_tor.x) + "\t\t" + str(vec_tor.y) + "\t\t" + str(vec_tor.z))
-		#print('')
-		#print (str(vec_rh.x-vec_tor.x) + "\t\t" + str(vec_lh.x-vec_tor.x) + "\t\t" + str(vec_rh.z-vec_tor.z) + "\t\t" + str(vec_lh.z-vec_tor.z))	
-		ukl = False
+		down = False
 		if ( (vec_rh.x > (vec_tor.x - 0.15) and vec_rh.x < vec_tor.x) \
 			 and  (vec_lh.x > (vec_tor.x - 0.15) and vec_lh.x < vec_tor.x) \
 			 and  (vec_rh.z > (vec_tor.z - 0.2) and vec_rh.z < (vec_tor.z + 0.2)) \
 			 and  (vec_lh.z > (vec_tor.z - 0.2) and vec_lh.z < (vec_tor.z+ 0.2))):
 			print("zlozone rece")
-			ukl = True;
-		if (ukl):
+			down = True;
+		if (down):
 			starthead = vec_head.x
 			count = 0
-			while (ukl):
+			while (down):
 				trans_head, rot = self.tf.lookupTransform('/openni_depth_frame', self.j_head, rospy.Duration())
 				self.r.sleep()
 				vec_head = Vector3(*trans_head)
@@ -200,9 +170,8 @@ class Tracker():
 				if (count == 4 and starthead-currhead < 0.1):
 					print("uklon!")
 					self.nao_sensei()
-					ukl = False
+					down = False
 		self.r.sleep()
-		#print("out!")
 
     def goto_process(self):
 	while not rospy.is_shutdown():
@@ -238,7 +207,7 @@ class Tracker():
 		dy = math.sin(beta)*lenght_to_ground
 		#print(dy)
 
-		#punk na podlodze w stosunku do kinecta
+		#punkt na podlodze w stosunku do kinecta
 		pkt = Vector3()
 		pkt.z = 0
 		pkt.x = B.x-dx
@@ -262,14 +231,6 @@ class Tracker():
 		vec_lh = Vector3(*trans_lh)
 		vec_reh = Vector3(*trans_erh)
 		vec_leh = Vector3(*trans_elh)
-		#print(str(vec_rh.x) + "\t\t" + str(vec_rh.y) + "\t\t" + str(vec_rh.z))
-		#print(str(vec_reh.x) + "\t\t" + str(vec_reh.y) + "\t\t" + str(vec_reh.z))
-		#print(' ')
-		#print(str(vec_lh.x) + "\t\t" + str(vec_lh.y) + "\t\t" + str(vec_lh.z))
-		#print(str(vec_leh.x) + "\t\t" + str(vec_leh.y) + "\t\t" + str(vec_leh.z))
-		#print(' ')
-		#print(' ')
-		
 		#rece pod katem 90 stopni do powierzchni
 		if(  (-0.05 < (vec_rh.z - vec_reh.z) < 0.05) and (-0.05 < (vec_lh.z - vec_leh.z) < 0.05) and count == 0 ):
 			print("start")
