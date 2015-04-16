aef75ae3829bab28ea81abcf98bc44c97aaa3196
PatrykBogd <patrykbochun@gmail.com>
2015-04-16 10:48:05 +0200

    Added launch file

--- scripts/dtrack.py
+++ scripts/dtrack.py
@@ -2,6 +2,7 @@
 import rospy
 import tf
 import math
+from naoqi import ALProxy
 from std_msgs.msg import Header
 from geometry_msgs.msg import Vector3
 from geometry_msgs.msg import Quaternion
@@ -13,6 +14,17 @@ class Tracker():
 	rospy.init_node('tracker_mykinect', anonymous=True)
 	self.myFrame = 'kinect_link'
 	self.r = rospy.Rate(20)		#rospy.Rate(1) = 1Hz
+
+	ip = rospy.get_param('~ip', '10.104.16.50')
+	port = int(rospy.get_param('~port', '9559'))
+	#self.al = ALProxy("ALAutonomousLife", ip, port)
+	#self.postureProxy = ALProxy("ALRobotPosture", ip, port)
+	#self.motionProxy = ALProxy("ALMotion", ip, port)
+	#self.al.setState("disabled")
+	#self.postureProxy.goToPosture("StandInit", 0.5)
+	#for part in ["Head", "LArm", "RArm"]:
+	#	self.motionProxy.setStiffnesses(part, 1.0)
+	#	self.init_kinnect_pose()
 	self.tf = tf.TransformListener()
 	rospy.loginfo("Start tracking for 3s...")
         rospy.sleep(3.0)
@@ -56,6 +68,16 @@ class Tracker():
 	return map(lambda x: float(x)*math.pi/180.0, angles)
 
 	
+    def init_kinnect_pose(self):
+	self.motionProxy.setAngles(['LShoulderRoll', 'RShoulderRoll'], self.to_rad([57.9, -57.9]), 1.0);
+	self.motionProxy.setAngles(['LShoulderPitch', 'RShoulderPitch'], self.to_rad([-94.1, -94.1]), 1.0);
+	self.motionProxy.setAngles(['LElbowYaw', 'RElbowYaw'], self.to_rad([-11.5, 11.5]), 1.0);
+	self.motionProxy.setAngles(['LElbowRoll', 'RElbowRoll'], self.to_rad([-61.3, 61.3]), 1.0);
+	self.motionProxy.setAngles(['LWristYaw', 'RWristYaw'], self.to_rad([7.0, -7.0]), 1.0);
+	self.motionProxy.setAngles(['LHand', 'RHand'], self.to_rad([0.99, 0.99]), 1.0);
+
+    def to_rad(self, angles):
+	return map(lambda x: float(x)*math.pi/180.0, angles)
 
     def nao_hello(self)
 	self.postureProxy.goToPosture("Standing",1)
@@ -76,21 +98,23 @@ class Tracker():
 	self.count = 0
 	self.side = 0		#left 0 right 1
 	while not rospy.is_shutdown():
-		trans_head, rot_head = self.tf.lookupTransform('/openni_depth_frame', self.j_head, rospy.Duration())
+		trans_head, rot= self.tf.lookupTransform('/openni_depth_frame', self.j_head, rospy.Duration())
 		vec_head = Vector3(*trans_head)
-		trans_hand, rot_hand = self.tf.lookupTransform('/openni_depth_frame', self.j_l_hand, rospy.Duration())
+		trans_hand, rot = self.tf.lookupTransform('/openni_depth_frame', self.j_l_hand, rospy.Duration())
 		vec_hand = Vector3(*trans_hand)
-		trans_shoul, rot_shoul = self.tf.lookupTransform('/openni_depth_frame', self.j_l_shoul, rospy.Duration())
+		trans_shoul, rot = self.tf.lookupTransform('/openni_depth_frame', self.j_l_shoul, rospy.Duration())
 		vec_shoul = Vector3(*trans_shoul)
-		if (vec_hand.z > vec_shoul.z):
-			print('reka w gorze')
-			print(vec_shoul.y-vec_hand.y)
+		trans_torso, rot = self.tf.lookupTransform('/openni_depth_frame', self.j_torso, rospy.Duration())
+		vec_torso = Vector3(*trans_torso)
+		if (vec_hand.z > vec_torso.z):
+			#print('reka w gorze')
+			#print(vec_shoul.y-vec_hand.y)
 			if (not self.side):
 				if (vec_shoul.y-vec_hand.y > 0.1):
 					self.count+=1
 					self.side = 1
 			else:
-				if (vec_shoul.y-vec_hand.y < (-0.1) ):
+				if (vec_shoul.y-vec_hand.y < (-0.2) ):
 					self.count+=1
 					self.side = 0
 		if (self.count >= 4):
@@ -150,7 +174,7 @@ class Tracker():
 					print("uklon!")
 					ukl = False
 		self.r.sleep()
-		print("out!")
+		#print("out!")
 
     def goto_process(self):
 	while not rospy.is_shutdown():
@@ -191,7 +215,7 @@ class Tracker():
 		pkt.z = 0
 		pkt.x = B.x-dx
 		pkt.y = B.y +dy
-		#print (str(pkt.x) +"\t\t" +str(pkt.y))
+		print (str(pkt.x) +"\t\t" +str(pkt.y))
 		self.r.sleep()
 
     def tome_process(self):
@@ -248,10 +272,15 @@ class Tracker():
 if __name__ == '__main__':
     try:
         tr = Tracker()
-	tr.hello_process()
-	#tr.sensei_process()
-	#tr.goto_process()
-	#tr.tome_process()
+        task = rospy.get_param('~task', None)
+        if task == 'hello':
+	    tr.hello_process()
+	if task == 'sensei':
+            tr.sensei_process()
+	if task == 'goto':
+            tr.goto_process()
+	if task == 'tome':
+            tr.tome_process()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
